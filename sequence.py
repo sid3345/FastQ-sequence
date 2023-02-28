b08098b5642566c5f17cfa93ffa9b5b4df06bd67
Siddharth Sinha <sid3345@yahoo.in>
2023-02-28 21:53:34 +0000

    Main FastQ sequence class and yield instead of return

--- sequence.py
+++ sequence.py
@@ -6,8 +6,9 @@ Author: Siddharth Sinha (sid3345@gmail.com)
 '''
 
 import utility
+import sys
 
-# Description: Reads the input sequence file and returns the sequence object.
+# Description: Reads the input sequence file and yields the sequence object.
 # Input: Input file
 # Output: sequence object
 def create_FastQ_sequence(fname):
@@ -17,7 +18,15 @@ def create_FastQ_sequence(fname):
 
     try:
         while sequence.check_sequence(f):
-            return sequence
+            yield sequence
     finally:
         f.close()
 
+
+'''Class to save FastQ sequence ID, sequence letters and quality values.'''
+class FastQ_sequence():
+
+    def __init__(self, sequence_id=None, sequence_letter=None, quality=None):
+        self.sequence_id = sequence_id
+        self.sequence_letter = sequence_letter
+        self.quality = quality
