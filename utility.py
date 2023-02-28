f1ee00ca36775e3390a3d8b4282ff2684bee1ef8
Siddharth Sinha <sid3345@yahoo.in>
2023-02-28 21:00:08 +0000

    utility script to read input zipped file or input command

---
+++ utility.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+'''
+Description: Utility functions to read and write file.
+Author: Siddharth Sinha (sid3345@gmail.com)
+'''
+
+import sys
+import gzip
+
+# Input: Input file
+# Output: IO Handler
+def read_file(input_file):
+    if input_file is None:
+        io_handler = sys.stdin
+
+    elif input_file.endswith('.gz'): # Zip file
+        try:
+            # This reads the file directly without unzipping it
+            io_handler=gzip.open(input_file)
+
+        except:
+            sys.exit("Error reading gzipped file: " + input_file)
+    return io_handler
